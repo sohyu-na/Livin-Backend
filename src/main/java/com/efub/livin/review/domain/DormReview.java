85b79815c403720726865b1cd26dddf291073bde
laura-jung <166522604+laura-jung@users.noreply.github.com>
2025-11-20 19:02:55 +0900

    fix : User 연결 누락 부분 다시 추가

--- src/main/java/com/efub/livin/review/domain/DormReview.java
+++ src/main/java/com/efub/livin/review/domain/DormReview.java
@@ -17,7 +17,7 @@ import java.util.List;
 public class DormReview extends BaseEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)  //sql에서 ALTER TABLE dorm_review AUTO_INCREMENT = 10000; 과정 필요
     private Long id;
 
     // 기숙사 이름
@@ -32,10 +32,10 @@ public class DormReview extends BaseEntity {
     @Column(nullable = false)
     private String roomPeople;
 
-//    //FK : user_id
-//    @ManyToOne(fetch = FetchType.LAZY)
-//    @JoinColumn(name = "user_id", updatable = false, nullable = false)
-//    private User user;
+    //FK : user_id
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", updatable = false, nullable = false)
+    private User user;
 
     // 시설 평가
     @Enumerated(EnumType.STRING)
@@ -84,7 +84,8 @@ public class DormReview extends BaseEntity {
             BugRate bugRate,
             Integer finalRate,
             String review,
-            Boolean anonym
+            Boolean anonym,
+            User user
     ) {
         this.buildName = buildName;
         this.buildNum = buildNum;
@@ -96,6 +97,7 @@ public class DormReview extends BaseEntity {
         this.finalRate = finalRate;
         this.review = review;
         this.anonym = anonym;
+        this.user = user;
     }
 
     //이미지 추가하는 메서드
