e29542fbe11934dd4343cf7135c5cd18f2c7e644
sohyu-na <solim73@naver.com>
2025-11-22 16:07:39 +0900

    feat: logout api 추가

--- src/main/java/com/efub/livin/auth/controller/AuthController.java
+++ src/main/java/com/efub/livin/auth/controller/AuthController.java
@@ -1,11 +1,13 @@
 package com.efub.livin.auth.controller;
 
+import com.efub.livin.auth.domain.CustomUserDetails;
 import com.efub.livin.auth.dto.request.LoginRequest;
 import com.efub.livin.auth.dto.response.TokenResponse;
 import com.efub.livin.auth.service.AuthService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,4 +24,8 @@ public class AuthController {
     public ResponseEntity<TokenResponse> login(@Valid @RequestBody LoginRequest request){
         return ResponseEntity.ok(authService.login(request));
     }
+
+    @PostMapping("/logout")
+    public ResponseEntity<Void> logout(@AuthenticationPrincipal CustomUserDetails userDetails){
+        return ResponseEntity.noContent().build();    }
 }
