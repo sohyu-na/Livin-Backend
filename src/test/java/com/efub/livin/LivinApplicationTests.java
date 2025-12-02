819dd9c0dee0026bec626cc926d4b7a7934a8e4d
Hanrann6 <yookrann@ewhain.net>
2025-12-02 15:06:02 +0900

    [Test] HouseDetailService 단위 테스트

--- src/test/java/com/efub/livin/LivinApplicationTests.java
+++ src/test/java/com/efub/livin/LivinApplicationTests.java
@@ -1,8 +1,10 @@
 package com.efub.livin;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Disabled
 @SpringBootTest
 class LivinApplicationTests {
 
